729768e8176febca59476e9e86786211e28b7ac8
Jerin Mathew Vinu <jerinmathewvinu@gmail.com>
2025-11-03 22:34:08 +0530

    Delete Cybercrime_Prevention_Social_Media directory

--- Cybercrime_Prevention_Social_Media/backend/cyber/__init__.py
+++
@@ -1,9 +0,0 @@
-# cyber/__init__.py
-# This file makes the 'cyber' directory a Python package.
-# It allows Django to import settings, urls, wsgi, and asgi modules.
-
-# Optional: You can add version or app config here
-__version__ = "1.0.0"
-
-# This tells Django that this is the root package
-default_app_config = 'cyber.apps.CyberConfig'  # Optional, if you have apps.py
